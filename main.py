a8e104d6877c853656b0691e0249153736857c08
Buster Scruggs <youssefibrahim909@gmail.com>
2024-07-13 12:19:37 -0500

    menu done

--- main.py
+++ main.py
@@ -2,11 +2,41 @@ def main():
 
     print_header()
     print('hey')
+    print_footer(2)
+    
+    print("""What would you like to do?
+          1) View all pets
+          2) Add more pets
+          3) Update an existing pet.
+          4) Remove an existing pet
+          5) Search pets by name
+          6) Search pets by age
+          7) Exit program
+          """)
+    
+    case = int(input("Enter your choice: "))
+    
+    def switch_case(case):
+        if case == 1:
+            pass
+        if case == 2:
+            pass
+        if case == 3:
+            pass
+        if case == 4:
+            pass
+        if case == 5:
+            pass
+        if case == 6:
+            pass
+        if case == 7:
+            pass
 
 
 
 def print_footer(count):
-    pass
+    print(f"+{'-'*22}+")
+    print(f"{count} rows in set.")
 
 def print_header():
     
