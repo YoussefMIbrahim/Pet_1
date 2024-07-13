89c636ab6d8cb09190e74d9578b094d02bda541e
Buster Scruggs <youssefibrahim909@gmail.com>
2024-07-13 12:47:13 -0500

    updated stuff

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def main():
     print('hey')
     print_footer(2)
     
+    pet_list = []
+    
+    
     print("""What would you like to do?
           1) View all pets
           2) Add more pets
@@ -20,7 +23,24 @@ def main():
         if case == 1:
             pass
         if case == 2:
-            pass
+            
+            added_counter = 0
+            
+            while True:
+                
+                pet_string = input('add pet (name, age): ')
+                
+                if pet_string == 'done':
+                    break
+                
+                pet_info = parse_pet(pet_string)
+                print(pet_info)
+                
+                added_counter += 1
+            
+            print(f'{added_counter} pets added')
+                
+                
         if case == 3:
             pass
         if case == 4:
@@ -33,6 +53,8 @@ def main():
             pass
 
 
+def parse_pet(pet_string):
+    return pet_string.strip().split()
 
 def print_footer(count):
     print(f"+{'-'*22}+")
