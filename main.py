bcbc07ab734a40b5b28ba53d2e7ed26230cb980c
Buster Scruggs <youssefibrahim909@gmail.com>
2024-07-13 11:17:55 -0500

    added header function

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 def main():
 
-
+    print_header()
     print('hey')
 
+
+
+def print_footer(count):
+    pass
+
+def print_header():
+    
+    print(f"+{'-'*22}+")
+    print(f"| ID | Name      | AGE |")
+    print(f"+{'-'*22}+")
+
+    
 
 main()
